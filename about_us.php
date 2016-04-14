e351d4e9ac8626bc311db82bd99c11dd115ae0c7
Dilan Kamburugamuwa <dilushana@gmail.com>
2016-04-14 09:15:36 +0530

    Added files via upload

---
+++ about_us.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="menu_bar.css" />
+
+ <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet"  type="text/css" href="styles.css">
+<title>About Us</title>
+</head>
+
+<body>
+<br />
+<table width="1024" align="center">
+<tr>
+<td width="768">
+
+
+
+<font face="Georgia, Times New Roman, Times, serif" class="con01"> Need help?</font> <font class="con02"> Contact us </font> <font class="con03" face="Georgia, Times New Roman, Times, serif" > 011-2375375</font></td>
+
+</td></tr>
+
+</div>
+<div class="line_01">
+<tr bgcolor="#666666">
+<td colspan="2"></td ></tr>
+</div>
+<div class="Logo_line">
+<div class="Logo" >
+	<tr>
+	<td colspan="2" class="Logo_line">
+    <img src="img/Capture.JPG" width="300" height="180" />
+</div>
+    </td></tr>
+    </div>
+    <tr>
+        <td colspan="2">
+        <br />
+    <br />    </td></tr>
+    
+    
+                                
+                                
+                                <tr>
+                                <td colspan="2">
+                                <div id='cssmenu'>
+
+<?php include 'menu.php';?>                   </td></tr>
+                                
+                                
+                        
+   <div class="catagory" align="left">
+       <tr>
+       <td>
+    	<div align="left" class="catagory_02">
+            <ul>
+            
+            
+            	<h1 align="center">About Us </h1>
+             
+             
+ 
+
+                <div aria-activedescendant="s" align="center">
+                  <p align="left">Our Company by its name being, <strong><em>ABC (pvt) Ltd Company,</em></strong> has been able to fulfill the needs of its customers for more than a   number of decades by meeting their requirements with a great pleasure   and honour. With the service of our company, it has been able to provide   the customers who are looking forward to purchase the most suitable   mobile phone for their purposes. A variety of mobile phones have been   able to displayed to the customers in our show rooms and there by   providing the customers the best environment to select the most   appropriate mobile phone according to their requirements.</p>
+                  <p align="left">By introducing this website, our   company has stepped up one action forward by providing our customers to   achieve the best idea about how they can select their mobile phone   before stepping towards our show rooms. Therefore, it can be stated that   through this website our customers can clearly get an idea about what   kind of phone they need to purchase because we have provided diagrams of   the phones available within our show rooms including their   specifications and prices as well.</p>
+                  <p align="left">And also through our website we   have provided access to contact with customer care support via mail in   order to clarify doubts regarding any of the products displayed within   the website. Therefore, our customers have the ability to solve their   doubts within a very short period of time and we ensure that we hope to   provide our customers the best service to our maximum. </p>
+                  <p align="left">So, we welcome all the mobile   phone customers to visit our website and gain its service as we are   entitled to provide the best requirements ever in the mobile phone   market. </p>
+                  
+                  
+                  <p align="left">With Regards,     The <strong><em> ABC (pvt) Ltd. </em></strong> </p>
+                </div>
+                <h1 align="center">
+                  <p class=" 3D">&nbsp;</p>
+                </h1>
+         <div class="about">
+ 
+</div>
+
+ 
+    
+</table>           </td>
+           </tr>
+          </div>
+                      <tr></tr>
+                                 <tr></tr>
+                                            <tr></tr>
+           </tr>  </div>
+           
+                    
+                                      <div class="Futer_line" align="center">
+                                        <tr bgcolor="#CCCCCC" style="border-top:dashed" >
+                                        <td colspan="2">                                        </td ></tr>
+                                        </div>
+                                
+                                <div class="Futer" align="center">
+                             <p><b>ABC Phones company © All rights Reseverd </b></p>                            
+                              
+                              
+                              <td width="414" align="right">
+<div class="account_desc" align="right">
+				<ul>
+					<li><a href="index.php">Home</b></a></li>
+					<li><a href="about_us.php">About us</b></a></li>
+					<li><a href="contact_us.php">Contact Us</a></li>
+					<li><a href="index.php">Phones</a></li>
+					
+				</ul>
+  </div></td></tr></td></tr> 
+                               </div>
+</table>
+</body>
+</html>
