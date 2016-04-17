497dd797e39add82db3db4b8a7fefb92ff899777
Dexter Web Solutions <dilushana@gmail.com>
2016-04-17 23:21:28 +0530

    Update Delete_Product.php

--- Delete_Product.php
+++ Delete_Product.php
@@ -2,7 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Delete Product</title>
+<title>Delete Product || ShowroomChainManager
+</title>
 </head>
 
 <body>
