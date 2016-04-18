57cdbcd4412b71578888583f1fc7344852f1283e
Dilan Dil <DilanDil@users.noreply.github.com>
2016-04-18 11:44:30 +0530

    Query for call phone specifications...
    <!--This will call a Query for phone specification-->

---
+++ pd.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+<!--This will call a querry for phone specification-->
+<body>
+<form action="Product_Details.php" method="post">
+<label><input name="P_Model" type="hidden"  /></label></br>
+<input name="show" type="submit" value="show" />
+</form>
+</body>
+</html>
