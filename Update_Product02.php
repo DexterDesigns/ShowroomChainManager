dfaebdf1ba6f30c58c39268843f45878c806ec44
Dilan Dilushana <DilanDil@users.noreply.github.com>
2016-04-24 23:09:28 +0530

    Update Update_product02.php
    this will required to properly connects with the Database....

--- Update_Product02.php
+++ Update_Product02.php
@@ -18,6 +18,15 @@ include 'database.php';
 $sql = "UPDATE `product` SET P_Image='".$P_Image."', P_Specifications='".$P_Specifications."', P_Brand='".$P_Brand."', 
 	 P_Price='".$P_Price."' WHERE P_Model='".$P_Model."'";	
 
+// this will required to properly connects with the Database
+if (mysql_query($sql)){
+	echo" Selected record is updated ";
+	
+}else{
+	echo "Error  <br/>", mysql_error($conn);
+}
+
+mysql_close($conn);	
 
 
 ?>
