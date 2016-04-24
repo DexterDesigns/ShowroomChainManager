2e820c26570afd3e75b307e636def78acb4130c1
Dexter Web Solutions <dilushana@gmail.com>
2016-04-24 15:10:21 +0530

    Show room Sales Registration
    This view can use for the Showroom sales registration...

---
+++ ShowRoom_SalesTeam.php
@@ -0,0 +1,11 @@
+<html><body>
+<label><b> Show Room Staff / Sales Team</b>  </label>
+<br>
+<br>
+
+<button><a href="pd01.php">Product Details</a></button>
+<button><a href="sales.php">Add Sales </a></button>
+<button><a href="cq.php">Customer Query Details</a></button>
+<button><a href="LogOut.php">Log Out</a></button>
+</body>
+</html>
