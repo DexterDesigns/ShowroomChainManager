7873fecc06a942d8f9b3a4f4b158772e8529cbf0
Dilan Dilushana <DilanDil@users.noreply.github.com>
2016-05-01 22:19:15 +0530

    Store Room Function
    Store Room Function of the system...

---
+++ StoreRoom.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Store Room Staff</title>
+</head>
+
+<body><label><b> Store Room Staff</b>  </label>
+<br>
+<br>
+<button><a href="pd.php">Product Details</a></button>
+<button><a href="Add_Product.php">Add Product</a></button>
+<button><a href="Delete_Product.php">Delete Product</a></button>
+<button><a href="Update_Product.php">Update Product</a></button>
+<button><a href="LogOut.php">Log Out</a></button>
+</body>
+</html>
